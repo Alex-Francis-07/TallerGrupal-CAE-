42c174fc1ed5f6c61c9de630be3df0ef0242bcfa
Mark777g <mark.gonzalez@unl.edu.ec>
2025-10-19 15:33:05 -0500

    Agregar clases Pila

---
+++ src/main/java/edu/unl/cc/Dato.java
@@ -0,0 +1,36 @@
+package edu.unl.cc;
+/**
+ *
+ * @author Mark
+ */
+public class Dato {
+    private String tipoNota; // Tipos de notas.
+    private Ticket ticket;
+    private Object valorAnterior; // Estado anterior o nota eliminada.
+    private Object valorNuevo;   // Estado nuevo o nota agregada.
+
+    // Constructor.
+    public Dato(String tipoNota, Ticket ticket, Object valorAnterior, Object valorNuevo) {
+        this.tipoNota = tipoNota;
+        this.ticket = ticket;
+        this.valorAnterior = valorAnterior;
+        this.valorNuevo = valorNuevo;
+    }
+
+    // Getters
+    public String getTipoNota() {
+        return tipoNota;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public Object getValorAnterior() {
+        return valorAnterior;
+    }
+
+    public Object getValorNuevo() {
+        return valorNuevo;
+    }
+}
---
+++ src/main/java/edu/unl/cc/Pila.java
@@ -0,0 +1,35 @@
+package edu.unl.cc;
+/**
+ *
+ * @author Mark
+ */
+public class Pila {
+    private Node tope_pila;
+
+    private static class Node {
+        Dato dato;
+        Node siguiente_nodo;
+        Node(Dato dato) { this.dato = dato; }
+    }
+
+    public void push(Dato dato) {
+        Node nuevoNode = new Node(dato);
+        // Apunta al tope actual.
+        nuevoNode.siguiente_nodo = tope_pila;
+        //El nuevo nodo se pone en el tope.
+        tope_pila = nuevoNode;
+    }
+
+    public Dato pop() {
+        // Accion que pasa si la pila esta vacia.
+        if (tope_pila == null) return null;
+        Dato dato = tope_pila.dato;
+        tope_pila = tope_pila.siguiente_nodo;
+        return dato;
+    }
+
+    //Accion si el tope de la pila esta vacio o nulo.
+    public boolean estaVacia() {
+        return tope_pila == null;
+    }
+}
